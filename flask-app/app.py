337c59273612cc3a0494b83559c3d4ffb3add964
acc-test-devops <rparracandia@gmail.com>
2025-04-03 17:29:57 -0300

    add: acc page

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+@app.route('/asset/<path:filename>')
+def serve_asset(filename):
+    return send_from_directory('static/assets', filename)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
